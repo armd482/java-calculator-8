d9d43676a4130a936a0c65afd5120b1da1a4d25e
armd482 <armd482@naver.com>
2025-10-15 14:20:52 +0900

    feat: 입력 요청 화면 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,5 +1,12 @@
 package calculator.controller;
 
+import calculator.view.InputView;
+
 public class CalculatorController {
-  
+    InputView inputView = new InputView();
+
+    public void run() {
+        String numbers = inputView.getNumbers();
+        System.out.println("입력 값:"+numbers);
+    }
 }
--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,5 +1,10 @@
 package calculator.view;
 
 public class InputView {
-  
+    private static final String NUMBER_INPUT_MESSAGE = "덧셈할 문자열을 입력해주세요.";
+
+    public String getNumbers() {
+        System.out.println(NUMBER_INPUT_MESSAGE);
+        return camp.nextstep.edu.missionutils.Console.readLine();
+    }
 }
