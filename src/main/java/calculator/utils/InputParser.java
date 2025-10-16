f23976c51700c009e9884a8198a5b1620998ca39
armd482 <armd482@naver.com>
2025-10-16 15:32:58 +0900

    refactor: parser 함수 분리 및 일부 validator 수정

--- src/main/java/calculator/utils/InputParser.java
+++ src/main/java/calculator/utils/InputParser.java
@@ -1,21 +1,29 @@
 package calculator.utils;
 
-import calculator.view.OutputView;
+import java.util.List;
 
 public class InputParser {
-    private String input;
-    private String separator = "[,:]";
+    private final String input;
+    private final List<String> DEFAULT_SEPARATOR = List.of(",", ":");
 
-    private void addSeparatorRegex(String customSeparator) {
-        if(separator.contains(customSeparator)) {
-            OutputView.displayInputError("커스텀 기본자가 기본 구분자에 포함된 구분자입니다.");
+    private final String separatorRegex;
+
+    private void validateCustomSeparator(String customSeparator) {
+        if(DEFAULT_SEPARATOR.contains(customSeparator)) {
+            throw new IllegalArgumentException("커스텀 기본자가 기본 구분자에 포함된 구분자입니다.");
         }
 
         if(customSeparator.equals(".")) {
-            OutputView.displayInputError("설정할 수 없는 커스텀 기본자입니다.");
+            throw new IllegalArgumentException("설정할 수 없는 커스텀 기본자입니다.");
         }
+    }
 
-        separator = "[,:" + customSeparator + "]";
+    private String getSeparatorRegex(String customSeparator) {
+        String separators = String.join("", DEFAULT_SEPARATOR);
+        if(customSeparator != null) {
+            separators += customSeparator;
+        }
+        return "[" +  separators + "]";
     }
 
     public InputParser(String input) {
@@ -23,19 +31,22 @@ public class InputParser {
             String[] inputParts= input.split("\\\\n", 2);
 
             if(inputParts.length != 2) {
-                OutputView.displayInputError("잘못된 입력 형태입니다.");
-                return;
+                throw new IllegalArgumentException("잘못된 입력 형태입니다.");
             }
 
             String customSeparator = inputParts[0].substring(2);
+            validateCustomSeparator(customSeparator);
+
             this.input = inputParts[1];
-            addSeparatorRegex(customSeparator);
+            this.separatorRegex = getSeparatorRegex(customSeparator);
             return;
         }
+
         this.input = input;
+        this.separatorRegex = getSeparatorRegex(null);
     }
 
     public String[] splitInput() {
-        return this.input.split(separator);
+        return this.input.split(separatorRegex);
     }
 }
