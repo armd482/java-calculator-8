bc0b2bb7f515625d42d7f84bb8985255920f637b
armd482 <armd482@naver.com>
2025-10-16 14:19:20 +0900

    refactor: CalculatorModel 레코드 클래스로 변경

--- src/main/java/calculator/model/CalculatorModel.java
+++ src/main/java/calculator/model/CalculatorModel.java
@@ -3,12 +3,7 @@ package calculator.model;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
-public class CalculatorModel {
-    private final String[] numbers;
-
-    public CalculatorModel(String[] numbers) {
-        this.numbers = numbers;
-    }
+public record CalculatorModel(String[] numbers) {
 
     public String calculateSum() {
         BigDecimal result = Arrays.stream(numbers).map(BigDecimal::new).reduce(BigDecimal.ZERO, BigDecimal::add);
